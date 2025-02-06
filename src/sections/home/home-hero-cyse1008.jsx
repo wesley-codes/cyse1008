7d4e5e873122ceb0be0cb9fb097f98b9a00c5cd9
Meka <ezemikey12@gmail.com>
2025-02-06 11:47:04 -0500

    chore: Added button and custom component

--- src/sections/home/home-hero-cyse1008.jsx
+++ src/sections/home/home-hero-cyse1008.jsx
@@ -1,7 +1,42 @@
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import CustomComponent from './custom-component';
 export function HomeHeroCYSE1008() {
+  const theme = useTheme();
+  const [count, setCount] = useState(0);
+
   return (
-  <>
-  
-  </>
+    <>
+      {/* <div>Hello World</div> */}
+
+      <Box
+        component="h1"
+        display="flex"
+        flexWrap="wrap"
+        justifyContent="center"
+        alignItems="flex-start"
+        sx={{
+          ...theme.typography.h1,
+          my: 0,
+          mx: 'auto',
+          maxWidth: 780,
+          color: '#32C282',
+          fontFamily: theme.typography.fontSecondaryFamily,
+          [theme.breakpoints.up('md')]: { fontSize: 62, lineHeight: '90px' },
+        }}
+      >
+        Meka's store
+      </Box>
+
+      <Button variant="contained" onClick={() => setCount(count + 1)}>
+        {' '}
+        Click me! {count}
+      </Button>
+
+
+      <CustomComponent/>
+    </>
   );
 }
