72d880fe7e14ba8fbf79770f882b26122f6ad663
cliff <dunderjeep@gmail.com>
2025-03-03 12:55:12 -0500

    updated config files to run emulators

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -1,12 +1,18 @@
 import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from "firebase/storage";
+import { getStorage } from 'firebase/storage';
 import { CONFIG } from 'src/config-global';
 
 // ----------------------------------------------------------------------
+// Check if running on localhost
+const isLocalhost = typeof window !== 'undefined' && window.location.hostname === 'localhost';
 
 const isFirebase = CONFIG.auth.method === 'firebase';
+// Merge emulator config dynamically if on localhost
+const firebaseConfig = isLocalhost
+  ? { ...CONFIG.firebase, ...CONFIG.firebaselocal }
+  : CONFIG.firebase;
 
 export const firebaseApp = isFirebase ? initializeApp(CONFIG.firebase) : {};
 
